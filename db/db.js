13016e99eedede3e68a3de8fe86aaed747f5c91a
Baxter Ross <baxterross@gmail.com>
2022-10-17 19:34:27 -0700

    allowing non ssl connection on prod

--- db/db.js
+++ db/db.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'development') {
     connectionString = process.env.HEROKU_POSTGRESQL_IVORY_URL;
  } else {
     connectionString = process.env.HEROKU_POSTGRESQL_IVORY_URL;
-    useSsl = true;
+    useSsl = false;
  }
 
 // Creating a new database instance from the connection details:
